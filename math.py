7f7dcfadf2249401433005daa88a232666d4ff6b
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-02-17 09:37:49 +0000

    refactor math operator example

---
+++ math.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Integer Addition
+intAddition = 10 + 5
+print(intAddition)
+
+# Integer Subtraction
+intSubtraction = 10 - 5
+print(intSubtraction)
+
+# Integer Multiplication
+intMultiplication = 10 * 5
+print(intMultiplication)
+
+# Integer Division
+intDivision = 10 / 5
+print(intDivision)
+
+# Order of operations (BODMAS)
+# () ^ / * + - Brackets, Orders/pOwers, Division, Multiplication, Addition, Subtraction)
+number = 1 + 2 * 3 / 4.0
+print(number)
+
