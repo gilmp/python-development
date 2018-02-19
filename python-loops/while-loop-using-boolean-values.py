24d2925248b6eb20083d231b40d2ad3409bce42f
Paul Gilmartin <paul.gilmartin@travisperkins.co.uk>
2018-02-19 15:26:20 +0000

    Adding a while loop with a boolean value

---
+++ python-loops/while-loop-using-boolean-values.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# This is a while loop that uses a boolean value to True or False against the count 5 (count < 5) = boolean value to print 0,1,2,3,4 when it hits 5 the while loop will exit.
+count = 0
+while count < 5:
+    print(count)
+    count += 1 # This is the same as count = count + 1
