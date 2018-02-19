7c7860b98f99915d978d46d643761b87a682cab5
Paul Gilmartin <paul.gilmartin@travisperkins.co.uk>
2018-02-19 16:13:48 +0000

    new sscripts

---
+++ python-loops/for-loop-using-ranges.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# This is a for loop where x is locating the range of 5 from 0 index to 5 printing out 0,1,2,3,4
+for x in range(5):
+    print(x)
+
+# This is a for loop where x is defining the range of 3 and 6 then printing out 3,4,5
+for x in range(3,6):
+    print(x)
+# This is a for loop where x is in range of 3 ,8 and 2 then uses the add operator to define the range to print 3,5,7 
+for x in range(3, 8, 2:
+    print(x)
+
