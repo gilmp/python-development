6968f5e896c83be63eb61809ca60852faf1461b9
Paul Gilmartin <paul.gilmartin@travisperkins.co.uk>
2018-02-19 14:32:07 +0000

    Adding a new script that is using a list to do a for loop against each item

---
+++ python-loops/running-a-for-loop-for-a-givern-sequence.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# This is using a list to do a for loop against each value four times which then prints out four times.
+prime = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
