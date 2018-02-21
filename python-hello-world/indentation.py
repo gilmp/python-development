680e9c45892e906de932bda3d7e7fb94e137e4dc
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-02-21 01:33:09 +0000

    float/helloW - comment suggestions

--- python-hello-world/indentation.py
+++ python-hello-world/indentation.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+# Indentation is important. All code within a function, condition or loop MUST be intented.
+# All indentation must be the same. Expected indentation is 4 spaces.
+
 x = 1
 y = 10
 if x == 1 or y == 2:
     # Indented four spaces
     print("x and y is 1 2. ")
+
