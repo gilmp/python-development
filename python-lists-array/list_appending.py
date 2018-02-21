1a693cc0b2d26b8b5e3d85d1d941fe38b3e37b00
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-02-21 01:36:32 +0000

    moved additional_ops to list_appending

--- python-lists-array/list_appending.py
+++ python-lists-array/list_appending.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
-# This is defining a list of even number and odd numbers and cancatinating them together. 1,2,3,4,5,6,7,8 #
+# This is defining a list of even numbers and odd numbers and concatenating them together
+# allnumbers = 1,2,3,4,5,6,7,8
 even_numbers = [2,4,6,8]
 odd_numbers  = [1,3,5,7]
 allnumbers = odd_numbers + even_numbers
 print(allnumbers)
+
+# This is using a for loop to add the even numbers to the end of odd_numbers and print out the length of odd numbers
+# len = 8
 even_numbers = [2,4,6,8]
 odd_numbers  = [1,3,5,7]
 
-# This is using a for loop to append even numbers to the end of odd numbers and print out the length of odd numbers# 
 for x in even_numbers:
     odd_numbers.append(x)
 print(len(odd_numbers))
+
