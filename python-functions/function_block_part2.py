8d2538ca89f19305788c13040ad99c903f31b4ad
Paul Gilmartin <paul.gilmartin@travisperkins.co.uk>
2018-04-09 09:08:50 +0100

    New function defining with different arguements

---
+++ python-functions/function_block_part2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def my_function():
+    print("Hello my name is paul")
+
+def my_function_with_args(ssh-key-users, ssh-keys):
+    print("Hello, %s , From My function!, this is the key %s"%(ssh-key-users, ssh-keys))
+
+def sum_two_numbers(key2, key1):
+    return key2 + key1
+
+myfunction()
+
+myfunction_with_args("info", "infor-key")
+
+x = sum_two_numbers(29,31)
+
