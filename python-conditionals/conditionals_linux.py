d51ea915e8d51b7f8f2d2eb817d52068ad3f3072
hacker <paul.gilmartin@travisperkins.co.uk>
2018-07-25 21:49:51 +0100

    boto scripts

---
+++ python-conditionals/conditionals_linux.py
@@ -0,0 +1,43 @@
+!#/usr/bin/env python
+
+1 < 2
+
+2 > 0
+
+2 == 1
+
+this == this
+
+3.0 >= 3.0
+
+1 != 2
+
+1.0 == 1
+
+1.0 != 1
+
+if True:
+    print("something is true")
+
+if False:
+   print("something is true")
+
+
+name = "Kevin"
+if len(name) >= 5:
+   print("name is long")
+else:
+   print("name is short")
+
+name = "luke"
+if len(name) >= 5:
+   print("name is long")
+elif len(name) == 4:
+   print("name is average")
+else
+   print("name is short")
+
+if "test:
+    print("this is test")
+
+bool("test")
---
+++ python-loops/while-loop-control-flow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+count = 0
+while count < 10:
+    print("were counting")
+    count += 1
+
+while count > 10:
+      if count % 2 == 0
+          count += 1
+          continue
+      print("counting odd numbers %s % count)
+      count += 1
